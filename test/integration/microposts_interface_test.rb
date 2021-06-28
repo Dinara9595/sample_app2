ee1b26d46d57526f6c9258d77e09e82da74adf65
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-06-28 18:15:41 +0300

    Test for count microposts and tests for upload pictures

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     #Невалидная отправка формы
     assert_no_difference 'Micropost.count' do
       post microposts_path, micropost: { content: ""}
@@ -16,12 +17,18 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     #Валидная отправка формы
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails-logo.png', 'image/png')
     assert_difference 'Micropost.count', 1 do
-      post microposts_path, micropost: { content: content}
+      post microposts_path, micropost: { content: content, picture: picture}
     end
-    assert_redirected_to root_url
+
+    foo=assigns(:micropost)
+    assert foo.picture?
+    assert @user.microposts.first.picture?
+    assert_equal foo, @user.microposts.first
     follow_redirect!
     assert_match content, response.body
+
     #Удаление сообщения
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
@@ -31,6 +38,20 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     #Посещение профиля другого пользователя.
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    #Пользователь без микросообщений
+    other_user = users(:malory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 
 end
